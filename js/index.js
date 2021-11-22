695fb8df8531acd17de0e60020eae0f719ab6d05
hazrahothman <77723723+hazrahothman@users.noreply.github.com>
2021-11-22 14:08:14 -0500

    Delete js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-//This is first time doing Javascript code
-//
-console.log("Hello World");
-let name;
-    nameNameId =prompt('What is your name?');
-    alert('Hey! welcome to my javascript assignment 3!!!' + nameNameId);
