c1c37446500a4b3d792d7b24b72d65e9c8b25f90
hazrah othman <me@hazrahothman.com>
2021-11-29 23:33:21 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Welcomes the visitor with an alert
 //declared a function named greetUser()
-console.log(hello);
+
 function greetUser() {
   alert("WELCOME TO YOUR ALERT BOX!!!!!");
   //   Prompts the visitor for his/her name
@@ -53,23 +53,6 @@ function greetUser() {
   }
   document.getElementById("demo").innerHTML = txt;
 }
+
 //a. Validate both firstName and lastName to ensure:
 //i. a minimum length of two (2) characters
-function validateForm() {
-  var firstName = document.getElementById("firstName");
-  var lastName = document.getElementById("lastName");
-  const facilitatorName = document.getElementById("facilitator");
-  if (firstName.value.length <= 2 || lastName.value.length <= 2) {
-    alert("A minimum length of two (2) characters");
-    return false;
-  } else {
-    return true;
-  }
-  //Validate facilitator and constrain the possible valid values to
-//be those of the active facilitators for the course during this
-//semester. For example, if we have facilitators with the following first
-//names: “Laura”, “Fazil”, and “Harsh”, you would ensure that only
-//these values would pass validation
-  const facilitatorName = 'Fazil'
-document.getElementById("demo").innerHTML = facilitatorName;
-}
