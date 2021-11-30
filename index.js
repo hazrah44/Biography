09cc122012150ed192f52719efc3b3202355cf9d
hazrah othman <me@hazrahothman.com>
2021-11-30 01:39:46 -0500

    update

--- index.js
+++ index.js
@@ -53,6 +53,28 @@ function greetUser() {
   }
   document.getElementById("demo").innerHTML = txt;
 }
-
+//Assignment 4
 //a. Validate both firstName and lastName to ensure:
 //i. a minimum length of two (2) characters
+function validateForm() {
+  var firstName = document.getElementById("firstName");
+  var lastName = document.getElementById("lastName");
+  const facilitatorName = document.getElementById("facilitator");
+  if (firstName.value.length <= 2 || lastName.value.length <= 2) {
+    alert("A minimum length of two (2) characters");
+    return false;
+  } else {
+    return true;
+  }
+  //Validate facilitator and constrain the possible valid values to
+//be those of the active facilitators for the course during this
+//semester. For example, if we have facilitators with the following first
+//names: “Laura”, “Fazil”, and “Harsh”, you would ensure that only
+//these values would pass validation
+  const facilitatorName = 'Fazil'
+document.getElementById("demo").innerHTML = facilitatorName;
+}
+// Change heading:
+document.getElementById("myH").innerHTML = "My Website";
+// Change paragraph:
+document.getElementById("myP").innerHTML = 'Logic will get you from A to Z; imagination will get you everywhere by Albert Einstein';
