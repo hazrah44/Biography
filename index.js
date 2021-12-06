43c21e117501ce588081a422fb2f5f38dfafbd1f
hazrah othman <me@hazrahothman.com>
2021-12-06 10:12:18 -0500

    add

--- index.js
+++ index.js
@@ -78,3 +78,18 @@ function validateForm() {
  alert("Active Facilitator");
 // Change heading:
 document.getElementById("myH").innerHTML = "My Website";}
+
+//Assignment 5 - Part 1
+async function getDegrees(url) {
+  // fetch the url
+  await fetch(url)
+    //get your data here, and check for the response status. If it's not 200, throw an error
+    .then((response) => response.json())
+    .then((data) =>
+      document.write(
+        `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`
+      )
+    );
+}
+
+getDegrees("./src/degrees.json");
